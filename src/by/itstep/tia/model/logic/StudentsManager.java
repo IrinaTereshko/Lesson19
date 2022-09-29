a6e9c9782b37087d03957ee14acb56886d0db454
IrynaTereshko <i.tereshko@list.ru>
2022-09-29 11:37:38 +0300

    пустой массив и нуль

--- src/by/itstep/tia/model/logic/StudentsManager.java
+++ src/by/itstep/tia/model/logic/StudentsManager.java
@@ -4,6 +4,11 @@ public class StudentsManager {
 
     public static double calcAvgStudentMark(int[] marks) {
 
+       // проверка на пустой массив и отсутствие массива
+        if (marks == null || marks.length == 0 ) {
+            return -1;
+        }
+
         double s = 0;
 
         for (int i = 0; i < marks.length; i++) {
---
+++ test/by/itstep/tia/model/logic/StudentsManagerTest.java
@@ -0,0 +1,48 @@
+package by.itstep.tia.model.logic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StudentsManagerTest {
+
+
+
+    @Test
+    public void testCalculateAvgMarkPositive(){
+
+               int[] marks = {5,5,5,5,5};
+               double expected = 5.0;
+
+               double actual = StudentsManager.calcAvgStudentMark(marks);
+
+        Assert.assertEquals(expected, actual, 0.001);
+
+
+    }
+
+    @Test
+    public void testCalculateAvgMarkNegativeEmptyObject(){
+
+        int[] marks = {}; // int[] marks = new int[0]
+        double expected = -1;
+
+        double actual = StudentsManager.calcAvgStudentMark(marks);
+
+        Assert.assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void testCalculateAvgMarkNegativeWithNull(){
+
+// можно не создавать массив с null , а сразу его передать в методе
+        //int[] marks = null; // int[] marks = new int[0]
+
+        double expected = -1;
+
+// 2 возможности передать отсутствие данных
+      //  double actual = StudentsManager.calcAvgStudentMark(marks);
+        //  или
+        double actual = StudentsManager.calcAvgStudentMark(null);
+        Assert.assertEquals(expected, actual, 0);
+    }
+}
